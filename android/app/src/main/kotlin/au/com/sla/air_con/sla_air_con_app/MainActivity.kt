e277af599d1c1463e27a6ba01a0f1a51f80beb7f
Nasir <nasir.dev74@gmail.com>
2023-09-12 14:33:57 +0600

    initial commit

---
+++ android/app/src/main/kotlin/au/com/sla/air_con/sla_air_con_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package au.com.sla.air_con.sla_air_con_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ package/sla_air_con/lib/sla_air_con.dart
@@ -0,0 +1,7 @@
+library sla_air_con;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
